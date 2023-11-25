accb0e7c7feed8805324f9e8a7098f71ce0632c6
gacallea <gacallea@users.noreply.github.com>
2023-11-25 16:55:29 +0100

    more customization. AstroVim seems perfect! so far...

--- plugins/community.lua
+++ plugins/community.lua
@@ -4,30 +4,44 @@ return {
   -- example of importing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
 
+  -- colors and UI/UX
   { import = "astrocommunity.colorscheme.catppuccin" },
-  { import = "astrocommunity.pack.swift" },
+  { import = "astrocommunity.color.ccc-nvim" },
+  { import = "astrocommunity.color.mini-hipatterns" },
+  { import = "astrocommunity.utility.noice-nvim" },
+
+  -- languages support
+  -- { import = "astrocommunity.pack.swift" },
   { import = "astrocommunity.pack.haskell" },
   { import = "astrocommunity.pack.lua" },
   { import = "astrocommunity.pack.bash" },
   { import = "astrocommunity.pack.html-css" },
   { import = "astrocommunity.pack.json" },
-  { import = "astrocommunity.pack.toml" },
-  { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.toml" },
   { import = "astrocommunity.pack.typescript" },
   { import = "astrocommunity.pack.tailwindcss" },
-  --  { import = "astrocommunity.pack.python" },
-  --  { import = "astrocommunity.pack.docker" },
+  -- { import = "astrocommunity.pack.python" },
+  -- { import = "astrocommunity.pack.docker" },
+
+  -- lsp + diagnostics + testing + debugging
   { import = "astrocommunity.test.neotest" },
-  { import = "astrocommunity.utility.noice-nvim" },
-  { import = "astrocommunity.project.project-nvim" },
-  { import = "astrocommunity.markdown-and-latex.peek-nvim" },
   { import = "astrocommunity.indent.mini-indentscope" },
-  { import = "astrocommunity.git.octo-nvim" },
-  { import = "astrocommunity.git.diffview-nvim" },
-  { import = "astrocommunity.git.blame-nvim" },
-  { import = "astrocommunity.editing-support.nvim-devdocs" },
   { import = "astrocommunity.diagnostics.trouble-nvim" },
   { import = "astrocommunity.diagnostics.lsp_lines-nvim" },
-  { import = "astrocommunity.color.mini-hipatterns" },
+
+  -- editor and editing supprt
+  { import = "astrocommunity.project.project-nvim" },
+  { import = "astrocommunity.project.nvim-spectre" },
+  { import = "astrocommunity.editing-support.nvim-devdocs" },
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  { import = "astrocommunity.editing-support.rainbow-delimiters-nvim" },
+  { import = "astrocommunity.register.nvim-neoclip-lua" },
+  { import = "astrocommunity.markdown-and-latex.peek-nvim" },
+
+  -- git
+  { import = "astrocommunity.git.diffview-nvim" },
+  { import = "astrocommunity.git.blame-nvim" },
+  { import = "astrocommunity.git.octo-nvim" },
 }
--- plugins/user.lua
+++ plugins/user.lua
@@ -1,12 +1,109 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  -- "andweeb/presence.nvim",
-  -- {
-  --   "ray-x/lsp_signature.nvim",
-  --   event = "BufRead",
-  --   config = function()
-  --     require("lsp_signature").setup()
-  --   end,
-  -- },
+
+  -- [[ ZETTELKASTEN ]]
+  -- https://zettelkasten.de/introduction/
+  -- https://github.com/mickael-menu/zk
+  {
+    "mickael-menu/zk-nvim",
+    config = function()
+      require("zk").setup {
+        picker = "telescope",
+        lsp = {
+          config = {
+            cmd = { "zk", "lsp" },
+            name = "zk",
+          },
+          auto_attach = {
+            enabled = true,
+            filetypes = { "markdown" },
+          },
+        },
+      }
+    end,
+  },
+
+  -- [[ UI/UX ]]
+  {
+    "echasnovski/mini.map",
+    branch = "stable",
+    config = function()
+      require("mini.map").setup()
+      local map = require "mini.map"
+      map.setup {
+        integrations = {
+          map.gen_integration.builtin_search(),
+          map.gen_integration.diagnostic {
+            error = "DiagnosticFloatingError",
+            warn = "DiagnosticFloatingWarn",
+            info = "DiagnosticFloatingInfo",
+            hint = "DiagnosticFloatingHint",
+          },
+        },
+        symbols = {
+          encode = map.gen_encode_symbols.dot "4x2",
+        },
+        window = {
+          side = "right",
+          width = 25,
+          winblend = 15,
+          show_integration_count = false,
+        },
+      }
+    end,
+  },
+
+  -- [[ GIT ]]
+  {
+    "mattn/vim-gist",
+    dependencies = {
+      "mattn/webapi-vim",
+    },
+    event = "BufRead",
+    config = function()
+      vim.cmd "let g:gist_clip_command = 'pbcopy'"
+      vim.cmd "let g:gist_detect_filetype = 1"
+      vim.cmd "let g:gist_open_browser_after_post = 1"
+      vim.cmd "let g:gist_post_private = 1"
+    end,
+  },
+
+  {
+    "ruifm/gitlinker.nvim",
+    event = "BufRead",
+    config = function()
+      require("gitlinker").setup {
+        opts = {
+          add_current_line_on_normal_mode = true,
+          action_callback = require("gitlinker.actions").copy_to_clipboard,
+          print_url = false,
+          mappings = nil,
+        },
+      }
+    end,
+    dependencies = "nvim-lua/plenary.nvim",
+  },
+
+  -- [[ UTILS ]]
+  {
+    "ethanholz/nvim-lastplace",
+    event = "BufRead",
+    config = function()
+      require("nvim-lastplace").setup {
+        lastplace_ignore_buftype = {
+          "quickfix",
+          "nofile",
+          "help",
+        },
+        lastplace_ignore_filetype = {
+          "gitcommit",
+          "gitrebase",
+          "svn",
+          "hgcommit",
+        },
+        lastplace_open_folds = true,
+      }
+    end,
+  },
 }
