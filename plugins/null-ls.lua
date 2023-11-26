260bebf0b56c9260cb536bb3bdd96951f6e33afd
gacallea <gacallea@users.noreply.github.com>
2023-11-26 15:44:14 +0100

    standardjs ts-standard instead

--- plugins/null-ls.lua
+++ plugins/null-ls.lua
@@ -8,8 +8,19 @@ return {
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
     -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
     config.sources = {
+      -- disable prettierd for these filestypes
+      null_ls.builtins.formatting.prettierd.with {
+        disabled_filetypes = {
+          "markdown",
+          "typescript",
+          "javascript",
+          "tsx",
+        },
+      },
       -- Set a formatter
+      null_ls.builtins.formatting.standardjs,
       null_ls.builtins.diagnostics.standardjs,
+      null_ls.builtins.formatting.standardts,
       null_ls.builtins.diagnostics.markdownlint,
       null_ls.builtins.formatting.swiftformat,
       null_ls.builtins.diagnostics.swiftlint,
