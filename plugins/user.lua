6628f3ab348b9a68ab4e9af9a52793e7c1e231c0
gacallea <gacallea@users.noreply.github.com>
2023-11-29 00:04:01 +0100

    del old plugs, minimap unfinished for now. new gitlinker

--- plugins/user.lua
+++ plugins/user.lua
@@ -3,85 +3,55 @@ return {
   -- Add plugins, the lazy syntax
 
   -- [[ UI/UX ]]
-  {
-    "echasnovski/mini.map",
-    branch = "stable",
-    config = function()
-      require("mini.map").setup()
-      local map = require "mini.map"
-      map.setup {
-        integrations = {
-          map.gen_integration.builtin_search(),
-          map.gen_integration.diagnostic {
-            error = "DiagnosticFloatingError",
-            warn = "DiagnosticFloatingWarn",
-            info = "DiagnosticFloatingInfo",
-            hint = "DiagnosticFloatingHint",
-          },
-        },
-        symbols = {
-          encode = map.gen_encode_symbols.dot "4x2",
-        },
-        window = {
-          side = "right",
-          width = 25,
-          winblend = 15,
-          show_integration_count = false,
-        },
-      }
-    end,
-  },
+  -- {
+  --   "echasnovski/mini.map",
+  --   version = "*",
+  --   event = { "BufEnter" },
+  --   -- cmd = {
+  --   --   "MiniMap.open()",
+  --   --   "MiniMap.close()",
+  --   --   "MiniMaps.refresh()",
+  --   --   "MiniMap.toggle()",
+  --   --   "MiniMap.toggle_side()",
+  --   --   "MiniMap.toggle_focus()",
+  --   -- },
+  --   keys = {
+  --     { "<leader>um", "<cmd>MiniMap.toggle<CR>", desc = "Toggle minimap", mode = { "n" } },
+  --   },
+  --   config = function()
+  --     require("mini.map").setup()
+  --     local map = require "mini.map"
+  --     map.setup {
+  --       integrations = {
+  --         map.gen_integration.builtin_search(),
+  --         map.gen_integration.diagnostic {
+  --           error = "DiagnosticFloatingError",
+  --           warn = "DiagnosticFloatingWarn",
+  --           info = "DiagnosticFloatingInfo",
+  --           hint = "DiagnosticFloatingHint",
+  --         },
+  --       },
+  --       symbols = {
+  --         encode = map.gen_encode_symbols.dot "4x2",
+  --       },
+  --       window = {
+  --         side = "right",
+  --         width = 25,
+  --         winblend = 15,
+  --         show_integration_count = false,
+  --       },
+  --     }
+  --   end,
+  -- },
 
   -- [[ GIT ]]
   {
-    "mattn/vim-gist",
-    dependencies = {
-      "mattn/webapi-vim",
+    "linrongbin16/gitlinker.nvim",
+    event = "BufRead",
+    keys = {
+      { "<leader>gy", "<cmd>GitLink<CR>", desc = "Git link copy", mode = { "n", "v" } },
+      { "<leader>gz", "<cmd>GitLink!<CR>", desc = "Git link open", mode = { "n", "v" } },
     },
-    event = "BufRead",
-    config = function()
-      vim.cmd "let g:gist_clip_command = 'pbcopy'"
-      vim.cmd "let g:gist_detect_filetype = 1"
-      vim.cmd "let g:gist_open_browser_after_post = 1"
-      vim.cmd "let g:gist_post_private = 1"
-    end,
-  },
-
-  {
-    "ruifm/gitlinker.nvim",
-    event = "BufRead",
-    config = function()
-      require("gitlinker").setup {
-        opts = {
-          add_current_line_on_normal_mode = true,
-          action_callback = require("gitlinker.actions").copy_to_clipboard,
-          print_url = false,
-          mappings = nil,
-        },
-      }
-    end,
-    dependencies = "nvim-lua/plenary.nvim",
-  },
-
-  -- [[ UTILS ]]
-  {
-    "ethanholz/nvim-lastplace",
-    event = "BufRead",
-    config = function()
-      require("nvim-lastplace").setup {
-        lastplace_ignore_buftype = {
-          "quickfix",
-          "nofile",
-          "help",
-        },
-        lastplace_ignore_filetype = {
-          "gitcommit",
-          "gitrebase",
-          "svn",
-          "hgcommit",
-        },
-        lastplace_open_folds = true,
-      }
-    end,
+    opts = {},
   },
 }
