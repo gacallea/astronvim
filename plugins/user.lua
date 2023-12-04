7b83109ae974bb13fdfcf62943247ead4a37d850
gacallea <gacallea@users.noreply.github.com>
2023-12-04 12:33:07 +0100

    pr accepted

--- plugins/user.lua
+++ plugins/user.lua
@@ -1,15 +1,4 @@
 return {
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-
-  -- [[ GIT ]]
-  {
-    "linrongbin16/gitlinker.nvim",
-    event = "BufRead",
-    keys = {
-      { "<leader>gy", "<cmd>GitLink<CR>", desc = "Git link copy", mode = { "n", "v" } },
-      { "<leader>gz", "<cmd>GitLink!<CR>", desc = "Git link open", mode = { "n", "v" } },
-    },
-    opts = {},
-  },
 }
