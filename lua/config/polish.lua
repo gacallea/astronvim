d8bc0005d73bc28026a96154d10323be165d129e
gacallea <gacallea@users.noreply.github.com>
2023-12-07 11:07:05 +0100

    this should be it for cp-ing over from v3

--- lua/config/polish.lua
+++ lua/config/polish.lua
@@ -2,6 +2,34 @@
 -- augroups/autocommands and custom filetypes also this just pure lua so
 -- anything that doesn't fit in the normal config locations above can go here
 
+-- autoopen mini-map
+--     vim.api.nvim_create_augroup("autoopenminimap", { clear = true })
+--   vim.api.nvim_create_autocmd({ "BufEnter", "Filetype" }, {
+--     desc = "Open mini.map and exclude some filetypes",
+--     pattern = { "*" },
+--     group = "autoopenminimap",
+--     callback = function()
+--       local exclude_ft = {
+--         "qf",
+--         "aerial",
+--         "neo-tree",
+--         "toggleterm",
+--         "TelescopePrompt",
+--         "alpha",
+--         "netrw",
+--         "trouble",
+--       }
+--
+--       local map = require "mini.map"
+--       if vim.tbl_contains(exclude_ft, vim.o.filetype) then
+--         map.close()
+--       elseif vim.o.buftype == "" then
+--         map.open()
+--       end
+--     end,
+--   })
+-- end,
+
 -- Set up custom filetypes
 -- vim.filetype.add {
 --   extension = {
